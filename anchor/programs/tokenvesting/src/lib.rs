7d838f7cf5b959ab9d04f93bf18fcab4d2f1501a
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-08-25 21:37:08 +0530

    Init

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -8,63 +8,27 @@ declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 pub mod tokenvesting {
     use super::*;
 
-    pub fn close(_ctx: Context<CloseTokenvesting>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn decrement(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_sub(1).unwrap();
-        Ok(())
-    }
-
-    pub fn increment(ctx: Context<Update>) -> Result<()> {
-        ctx.accounts.tokenvesting.count = ctx.accounts.tokenvesting.count.checked_add(1).unwrap();
-        Ok(())
-    }
-
-    pub fn initialize(_ctx: Context<InitializeTokenvesting>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-        ctx.accounts.tokenvesting.count = value.clone();
+    pub fn create_vesting_account(context: Context<CreateVestingAccount>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct InitializeTokenvesting<'info> {
+pub struct CreateVestingAccount<'info> {
     #[account(mut)]
-    pub payer: Signer<'info>,
+    pub signer: Signer<'info>,
 
     #[account(
-  init,
-  space = 8 + Tokenvesting::INIT_SPACE,
-  payer = payer
+        init,
+        payer = signer,
+        space = 8 
     )]
-    pub tokenvesting: Account<'info, Tokenvesting>,
-    pub system_program: Program<'info, System>,
-}
-#[derive(Accounts)]
-pub struct CloseTokenvesting<'info> {
-    #[account(mut)]
-    pub payer: Signer<'info>,
-
-    #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
-    )]
-    pub tokenvesting: Account<'info, Tokenvesting>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-    #[account(mut)]
-    pub tokenvesting: Account<'info, Tokenvesting>,
 }
 
 #[account]
-#[derive(InitSpace)]
-pub struct Tokenvesting {
-    count: u8,
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    pub company_name: String,
 }
