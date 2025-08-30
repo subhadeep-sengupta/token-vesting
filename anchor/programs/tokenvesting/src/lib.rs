9463a6cc2f2111e0c596d7fddfd719c2b13c712e
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-08-30 22:52:42 +0530

    Claim Tokens Fucntion is done!

--- anchor/programs/tokenvesting/src/lib.rs
+++ anchor/programs/tokenvesting/src/lib.rs
@@ -4,7 +4,7 @@ use anchor_lang::prelude::*;
 
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{Mint, TokenAccount, TokenInterface},
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
 };
 
 declare_id!("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
@@ -50,7 +50,69 @@ pub mod tokenvesting {
         Ok(())
     }
 
-    pub fn claim_tokens(context: Context<ClaimTokens>, company_name: String) -> Result<()> {
+    pub fn claim_tokens(context: Context<ClaimTokens>, _company_name: String) -> Result<()> {
+        let employee_account = &mut context.accounts.employee_account;
+
+        let now = Clock::get()?.unix_timestamp;
+
+        if now < employee_account.cliff_time {
+            return Err(CustomErrorCode::ClaimNotAvailableYet.into());
+        }
+
+        let time_since_start = now.saturating_sub(employee_account.start_time);
+
+        let total_vesting_time = employee_account
+            .end_time
+            .saturating_sub(employee_account.start_time);
+
+        if total_vesting_time == 0 {
+            return Err(CustomErrorCode::InvalidVestingPeriod.into());
+        }
+
+        let vested_amount = if now >= employee_account.end_time {
+            employee_account.total_amount
+        } else {
+            match employee_account
+                .total_amount
+                .checked_mul(time_since_start as u64)
+            {
+                Some(product) => product / total_vesting_time as u64,
+                None => {
+                    return Err(CustomErrorCode::CalculationOverflow.into());
+                }
+            }
+        };
+
+        let claimable_amount = vested_amount.saturating_sub(employee_account.total_withdrawn);
+
+        if claimable_amount == 0 {
+            return Err(CustomErrorCode::NothingToClaim.into());
+        }
+
+        let transfer_cpi_accounts = TransferChecked {
+            from: context.accounts.treasury_token_account.to_account_info(),
+            mint: context.accounts.mint.to_account_info(),
+            to: context.accounts.employee_token_account.to_account_info(),
+            authority: context.accounts.treasury_token_account.to_account_info(),
+        };
+
+        let cpi_program = context.accounts.token_program.to_account_info();
+
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"vesting_treasury",
+            context.accounts.vesting_account.company_name.as_ref(),
+            &[context.accounts.vesting_account.treasury_bump],
+        ]];
+
+        let cpi_context =
+            CpiContext::new_with_signer(cpi_program, transfer_cpi_accounts, signer_seeds);
+
+        let decimals = context.accounts.mint.decimals;
+
+        transfer_checked(cpi_context, claimable_amount as u64, decimals)?;
+
+        employee_account.total_withdrawn += claimable_amount;
+
         Ok(())
     }
 }
@@ -77,7 +139,7 @@ pub struct CreateVestingAccount<'info> {
     #[account(
         init,
         token::mint = mint,
-        token::authority = vesting_account,
+        token::authority = treasury_token_account,
         payer = signer,
         seeds = [b"vesting_treasury", company_name.as_bytes()],
         bump
@@ -124,7 +186,7 @@ pub struct ClaimTokens<'info> {
         bump = employee_account.bump,
         has_one = vesting_account
     )]
-    pub employee_account: Accoount<'info, EmployeeAccount>,
+    pub employee_account: Account<'info, EmployeeAccount>,
 
     #[account(
         mut,
@@ -179,4 +241,18 @@ pub struct EmployeeAccount {
     pub total_amount: u64,
     pub total_withdrawn: u64,
     pub bump: u8,
+}
+
+#[error_code]
+pub enum CustomErrorCode {
+    #[msg("Claim not available yet")]
+    ClaimNotAvailableYet,
+
+    #[msg("Invalid Vesting Period")]
+    InvalidVestingPeriod,
+
+    #[msg("")]
+    CalculationOverflow,
+    #[msg("Nothing to claim")]
+    NothingToClaim,
 }
