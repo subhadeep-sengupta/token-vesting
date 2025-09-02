08baa660d747ee0a3c39ea16548720fabcd3909c
Subhadeep Sengupta <www.subhadeepsengupta2002@gmail.com>
2025-09-02 11:17:21 +0530

    All tests are written

--- anchor/tests/bankrun.spec.ts
+++ anchor/tests/bankrun.spec.ts
@@ -1,9 +1,9 @@
 import { describe, beforeAll, it } from "vitest";
 import * as anchor from "@coral-xyz/anchor"
 import { PublicKey, type Keypair } from "@solana/web3.js"
-import { BanksClient, ProgramTestContext, startAnchor } from "solana-bankrun"
+import { BanksClient, Clock, ProgramTestContext, startAnchor } from "solana-bankrun"
 import { BankrunProvider } from "anchor-bankrun"
-import { createMint } from "spl-token-bankrun"
+import { createMint, mintTo } from "spl-token-bankrun"
 import { Program } from "@coral-xyz/anchor"
 import IDL from "../target/idl/tokenvesting.json"
 import { Tokenvesting } from "../target/types/tokenvesting"
@@ -107,7 +107,7 @@ describe("Vesting Smart Contract Test", () => {
 	}, 50000)
 
 	it("should create a vesting account✅", async () => {
-		const tx = await program.methods.createVestingAccount(companyName).accounts({
+		const createVestingAccountTx = await program.methods.createVestingAccount(companyName).accounts({
 			signer: employer.publicKey,
 			mint,
 			tokenProgram: TOKEN_PROGRAM_ID,
@@ -118,9 +118,67 @@ describe("Vesting Smart Contract Test", () => {
 
 		console.log("Vesting Account Data:", vestingAccountData, null, 2)
 
-		console.log("Create Vesting Account:", tx)
+		console.log("Create Vesting Account:", createVestingAccountTx)
+	});
+
+	it("Should fund the treasury token account🖼️", async () => {
+		const amount = 10_000 * 10 ** 9;
+		const mintTx = await mintTo(
+			// @ts-expect-error: Type Error in spl-token-bankrun dependency
+			banksClient,
+			employer,
+			mint,
+			treasuryTokenAccount,
+			employer,
+			amount
+		);
+
+		console.log("Mint Treasury Token Account:", mintTx)
+	});
+
+	it("Should Create an employee vesting token account", async () => {
+		const createEmployeeAccountTx = await program.methods.createEmployeeAccount(
+			new anchor.BN(0),
+			new anchor.BN(100),
+			new anchor.BN(100),
+			new anchor.BN(0),
+		).accounts({
+			beneficiary: beneficiary.publicKey,
+			vestingAccount: vestingAccountkey,
+		}).rpc({ commitment: "confirmed", skipPreflight: true });
 
 
+		console.log("Created Employee Token Account:", createEmployeeAccountTx)
+		console.log("Employee Account:", employeeAccount.toBase58())
+	});
 
+	it("Should Claim Tokens", async () => {
+
+		await new Promise(resolve => setTimeout(resolve, 3000));
+
+		console.log("Waited")
+
+		const currrentClock = await banksClient.getClock()
+
+		console.log("Got the current clock")
+
+		context.setClock(
+			new Clock(
+				currrentClock.slot,
+				currrentClock.epochStartTimestamp,
+				currrentClock.epoch,
+				currrentClock.leaderScheduleEpoch,
+				1000n
+			)
+		);
+
+		console.log(context)
+
+		const claimTokensTx = await program2.methods
+			.claimTokens(companyName)
+			.accounts({ tokenProgram: TOKEN_PROGRAM_ID })
+			.rpc({ commitment: "confirmed" });
+
+		console.log("Claim Tokens Tx:", claimTokensTx)
 	})
 })
